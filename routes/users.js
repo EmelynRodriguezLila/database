3f3a4bd6c77feb3c5ffa1324f8721154f41873d6
Emelyn Rodriguez Lila <emelynlila04li@gmail.com>
2023-10-11 13:21:10 -0600

    Manejo de ENV y nuevo Endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,15 @@
 const {Router} = require('express');
-const {listUsers} = require('../controllers/users')
+const {listUsers, listUserByID} = require ('../controllers/users')
 
 const router = Router();
 
-//http://localhost:3000/api/v1/users/
-router.get('/', listUsers);
-//router.post('/', listUsers);
-//router.put('/', listUsers);
-//router.patch('/', listUsers);
-//router.delete('/', listUsers);
+//http://localhost:3000/api/v1/users/??
+router.get('/',listUsers);
+//se coloca dos puntos porque id es un valor variable
+router.get('/:id',listUserByID);
+//router.post('/',listUsers);
+//router.put('/',listUsers);
+//router.patch('/',listUsers);
+//router.delete('/',listUsers);
 
-module.exports = router;
+module.exports= router;
